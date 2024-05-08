e29f31233bcbb4f6218332a5995e7a0a3c4e0a64
Uraniamoon <cam.henriquezo@duocuc.cl>
2024-05-07 20:12:49 -0400

    Add files via upload
    camila h

---
+++ camilah.py
@@ -0,0 +1,32 @@
+print("bienvenido a sushi otaku:")
+pikachu=4500
+otaku=5000
+pulpo=5200
+anguila= 4800
+opcion=0
+
+while 
+   
+   print("1.Pikachu roll $4500:")
+   print("2.Otaku roll $5000:")
+   print("3.Pulpo venenoso roll $5200:")
+   print("4.Anguila electrica roll $4800:")
+   
+
+  opcion=input int("seleccione una opcion(1-4)":))
+                
+      if op=="1"
+        print (f("escogiste pikachu roll:"))
+      elif op== "2"
+        print(f("escogiste otaku roll:"))
+      elif op=="3"
+        print(f("escogiste nuestro pulpo venenoso:"))
+      elif op==4
+        print(f("escogiste nuestra anguila electrica:"))
+
+
+    
+
+
+
+
